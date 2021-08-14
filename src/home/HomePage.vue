c540fc2bb44ad141a3bef6d447236781d43515e9
ptanlam <phanlam20033002@gmail.com>
2021-08-14 20:40:05 +0700

    initial commit

---
+++ src/home/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div>
+      <img src="../assets/robot-home.png" alt="" class="robot" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your first robot!
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomePage',
+    props: {
+      msg: String,
+    },
+  };
+</script>
+
+<style scoped>
+  .home {
+    text-align: center;
+  }
+
+  .robot {
+    height: 300px;
+  }
+</style>
